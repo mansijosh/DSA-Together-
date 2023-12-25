e7a94600967aee6fb1da62e972e3b8f4c5d53148
Mansi Joshi <115941577+mansijosh@users.noreply.github.com>
2023-12-25 23:51:33 +0530

    Create Determinant of a Matrix.cpp

---
+++ Determinant of a Matrix.cpp
@@ -0,0 +1,28 @@
+//Given a square matrix of size n*n. The task is to find the determinant of this matrix.
+
+
+class Solution
+{   
+    public:
+    //Function for finding determinant of matrix.
+    int determinantOfMatrix(vector<vector<int> > matrix, int n)
+    {
+        // code here 
+        if(n==1) return matrix[0][0];
+        if(n==2) return matrix[0][0]*matrix[1][1]-matrix[1][0]*matrix[0][1];
+        int ans=0;
+        int sign=1;
+        for(int i=0;i<n;i++){
+            vector<vector<int>> temp(n-1,vector<int>(n-1));
+            int row = 0;
+            for(int col=0;col<n;col++){
+                if(col==i) continue;
+                for(int k=1;k<n;k++) temp[k-1][row]=matrix[k][col];
+                row++;
+            }
+            ans+=sign*matrix[0][i]*determinantOfMatrix(temp,n-1);
+            sign=-sign;
+        }
+        return ans;
+    }
+};
