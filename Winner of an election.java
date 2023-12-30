7b511839bc64da851489d3e2553427e2683ede0b
Mansi Joshi <115941577+mansijosh@users.noreply.github.com>
2023-12-30 22:25:27 +0530

    Create Winner of an election.java

---
+++ Winner of an election.java
@@ -0,0 +1,34 @@
+/*Given an array of n names arr of candidates in an election, where each name is a string of lowercase characters. A candidate name in the array represents a vote casted to the candidate. Print the name of the candidate that received the maximum count of votes. If there is a draw between two candidates, 
+  then print lexicographically smaller name.*/
+
+
+
+class Solution
+{
+    //Function to return the name of candidate that received maximum votes.
+    public static String[] winner(String arr[], int n)
+    {
+        // add your code
+        HashMap<String,Integer> hm = new HashMap<>();
+        String res[] = new String[2];
+        
+        int max = 0;
+        
+        for(int i=0;i<n;i++){
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+            max = Math.max(max,hm.get(arr[i]));
+            
+        }
+        
+        for(String key : hm.keySet()){
+            if(hm.get(key)==max){
+                if(res[0]==null) res[0]=key;
+                else{
+                    if(res[0].compareTo(key)>0) res[0] = key;
+                }
+            }
+        }
+        res[1] = String.valueOf(max);
+        return res;
+    }
+}
