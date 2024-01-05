fd7032c472f670991f9ef4122d52cb519f3b8dde
Mansi Joshi <115941577+mansijosh@users.noreply.github.com>
2024-01-05 23:27:22 +0530

    Create Count possible ways to construct buildings.java

---
+++ Count possible ways to construct buildings.java
@@ -0,0 +1,29 @@
+/*There is a road passing through a city with N plots on both sides of the road. Plots are arranged in a straight line on either side of the road. Determine the total number of ways to construct buildings in these plots, ensuring that no two buildings are adjacent to each other. Specifically, buildings on opposite sides of the road cannot be adjacent.
+
+Using * to represent a plot and || for the road, the arrangement for N = 3 can be visualized as follows: * * * || * * *.
+
+Note: As the answer can be very large, print it mod 109+7.
+
+*/
+
+
+
+class Solution
+{
+    public int TotalWays(int N)
+    {
+        // Code here
+        int mod = 1000000007;
+        long a=1,b=2,c=2;
+        
+        for(int i=2; i<= N; i++){
+            c = (a+b)%mod;
+            a = b;
+            b = c;
+        }
+        
+        c = (c*c)%mod;
+        return (int)c;
+    }
+}
+
